c17fb586180d1c04785789d59b9376c2da7d0721
luccy-92 <lucy9_0@hotmail.com>
2024-10-24 22:16:14 -0400

    cambios

--- src/app/pages/buscar/buscar.component.ts
+++ src/app/pages/buscar/buscar.component.ts
@@ -2,14 +2,37 @@ import { Component, inject, OnInit } from '@angular/core';
 import { HeaderService } from '../../core/services/header.service';
 
 @Component({
-  selector: 'app-buscar',
-  templateUrl: './buscar.component.html',
-  styleUrls: ['./buscar.component.css']
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css'],
 })
-export class BuscarComponent {
-  headerService = inject(HeaderService);
+export class SearchComponent implements OnInit {
+  query: string = '';
+  results: any[] = [];
+  categories: string[] = ['Entrantes', 'Platos principales', 'Postres', 'Bebidas'];
+  selectedCategory: string = '';
+  filteredResults: any[] = [];
 
-  ngOnInit(): void {
-      this.headerService.titulo.set("Buscar")
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {}
+
+  onSearch() {
+    this.http.get<any[]>(`/api/restaurant/search?query=${this.query}`).subscribe((data) => {
+      this.results = data;
+      this.applyFilters();
+    });
+  }
+
+  applyFilters() {
+    this.filteredResults = this.results.filter(item => {
+      const matchesCategory = this.selectedCategory ? item.category === this.selectedCategory : true;
+      const matchesPrice = item.price <= 20; // Por ejemplo, filtrar por un precio máximo
+      return matchesCategory && matchesPrice;
+    });
+  }
+
+  onCategoryChange() {
+    this.applyFilters();
   }
 }
